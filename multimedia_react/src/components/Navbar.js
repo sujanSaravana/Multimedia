ccbce81476e6830c1044d1f0abe1dc696924db89
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-15 21:35:55 +0200

    Navbar -> Browserrouter verlinkt

--- multimedia_react/src/components/Navbar.js
+++ multimedia_react/src/components/Navbar.js
@@ -1,39 +1,61 @@
 import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
-import impress from '../pages/impress';
-import kontakt from '../pages/kontakt';
-import landingpage from '../pages/landingpage';
-import uebermich from '../pages/ueber_mich';
-import angebote from '../pages/angebote';
+import Impress from '../pages/impress';
+import Kontakt from '../pages/kontakt';
+import Landingpage from '../pages/landingpage';
+import Uebermich from '../pages/ueber_mich';
+import Angebote from '../pages/angebote';
 import '../App.css';
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
-import NavDropdown from "react-bootstrap/NavDropdown"
-
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Bar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    <Navbar className="navbar" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Lumio💡</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav">
-            <Nav.Link href="#landingpage">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <BrowserRouter>
+      <Navbar className="navbar" expand="lg" expanded={expanded}>
+        <Container>
+          <Navbar.Brand as={Link} to="/Landingpage" className="logo">
+            Lumio💡
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={handleToggle}
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="nav">
+              <NavDropdown title="Produkte" id="basic-nav-dropdown" className="nav-title-dropdown">
+                <NavDropdown.Item href="#action/3.1">Decklampen</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">...Lampen</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link as={Link} to="/Kontakt">
+                <p className="nav-title">Kontakt</p>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Uebermich">
+                <p className="nav-title">Über mich</p>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Impress">
+                <p className="nav-title">Impressum & Datenschutz</p>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/Landingpage" element={<Landingpage />} />
+        <Route path="/Angeote" element={<Angebote />} />
+        <Route path="/Uebermich" element={<Uebermich />} />
+        <Route path="/Impress" element={<Impress />} />
+        <Route path="/Kontakt" element={<Kontakt />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
