525f25c9294835bd5a74a2b5e16c88b01892941d
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-27 19:00:40 +0200

    Kontakt form

--- multimedia_react/src/pages/kontakt.js
+++ multimedia_react/src/pages/kontakt.js
@@ -1,15 +1,43 @@
-import React from "react";
-import MyParallax from "../components/Parallax";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function kontakt() {
-    return (
-        <div>
-            <h1> 
-            kontakt
-            </h1>
-            <MyParallax />
-        </div>
-    );
+function Kontakt() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_ji390dw',
+        'template_wluxotj',
+        form.current,
+        'KR7Sqx23E4bH_nzvL'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent")
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
 }
 
-export default kontakt;
+export default Kontakt;
