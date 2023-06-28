ab10a7842c2c66bb55e216e92199493fe50502ed
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-28 21:32:46 +0200

    before-after image comparision

--- multimedia_react/src/components/ImageCompare.js
+++ multimedia_react/src/components/ImageCompare.js
@@ -1,22 +1,31 @@
 import React from "react";
 import ReactCompareImage from 'react-compare-image';
-import bild12 from '../Images/12.jpg';
+import bild12 from '../Images/13v2.png';
 import bild13 from '../Images/13.jpg';
 import bild14 from '../Images/14.jpg'
 import '../App.css';
 import bild15 from '../Images/15.jpg'
+import { Col, Container, Row } from "react-bootstrap";
 
 const ImageCompare = () => {
     return(
-        <div>
-            <ReactCompareImage
-      leftImage={bild12}
-      rightImage={bild13}
-      sliderLineWidth={20} 
-      sliderLineColor="#258306" 
-    />
-        </div>
+        <Container fluid className="compare-background" >
+            <div className="image-compare-row">
+        <Row >
+            <Col className="image-compare-col">
+                <ReactCompareImage className="mi12" leftImage={bild12} rightImage={bild13} sliderLineWidth={5}  sliderLineColor="#fff" />
+            </Col>
+            <Col className="image-compare-col">
+                <ReactCompareImage leftImage={bild12} rightImage={bild13} sliderLineWidth={5}  sliderLineColor="#fff" />
+            </Col>
+            <Col className="image-compare-col">
+                <ReactCompareImage leftImage={bild12} rightImage={bild13} sliderLineWidth={5}  sliderLineColor="#fff" />
+            </Col>
+        </Row>
+            </div>
+        </Container>
     )
 }
 
 export default ImageCompare;
+
