2283c622f83d2c4b62565b54687150d1e9aedf0a
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-28 22:37:09 +0200

    started about us page

--- multimedia_react/src/components/Product1.js
+++ multimedia_react/src/components/Product1.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import '../App.css';
+import bild12 from '../Images/12.jpg';
+import Image from "react-bootstrap/Image"
 import { Container } from "react-bootstrap";
 
 function Product1() {
     return(
         <Container fluid className="background">
         <div>
-
+        <Image fluid src={bild12} alt='Img 1' className='gallery-img'></Image>
         </div>
         </Container>
     )
--- multimedia_react/src/pages/ueber_mich.js
+++ multimedia_react/src/pages/ueber_mich.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
 
 function uebermich() {
     return (
+        <Container fluid className="background">
         <div>
-            <h1> 
-            ueber_mich
+            <h1 className="title"> 
+                Our Story 
             </h1>
+            <Row className="upper-background">
+                <h1>jlwd</h1>
+            </Row>
+            <Row className="lower-background">
+                <h1>jdf</h1>
+            </Row>
         </div>
+        </Container>
     );
 }
 
