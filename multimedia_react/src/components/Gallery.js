e5dce527ae497e2c3ac6565d7ac7ea060e8db68f
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-28 22:23:08 +0200

    product gallery

--- multimedia_react/src/components/Gallery.js
+++ multimedia_react/src/components/Gallery.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Grid from "react-bootstrap/Container"
+import { Link } from "react-router-dom";
 import Image from "react-bootstrap/Image"
 import '../App.css';
 import Container from "react-bootstrap/Container";
+import Product1 from "./Product1";
 import bild12 from '../Images/12.jpg';
 import bild13 from '../Images/13.jpg';
 import bild14 from '../Images/14.jpg'
@@ -15,25 +17,43 @@ function ProductGallery(){
         <div>
             <Container fluid>
             <Row>
+                <h1 className="title">Gallery</h1>
                 <Col xs={6} md={4}>
+                    <Link to="./Product1">
+                        <div className="galleryImage">
+                            <Image fluid src={bild12} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                    </Link>
+                        <div className="gallImage">
+                            <Image fluid src={bild15} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                        <div className="gallImage">
+                            <Image fluid src={bild13} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                </Col>
+                <Col xs={6} md={4}>
+                        <div className="galleryImage">
+                            <Image fluid src={bild13} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                        <div className="gallImage">
+                            <Image fluid src={bild12} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                        <div className="gallImage">
+                            <Image fluid src={bild15} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                </Col>
+                <Col xs={6} md={4}>
+                    <Link to="/Landingpage">
+                        <div className="galleryImage">
                     <Image fluid src={bild12} alt='Img 1' className='gallery-img'></Image>
-                </Col>
-                <Col xs={6} md={4}>
-                    <Image fluid src={bild13} alt='Img 1' className='gallery-img'></Image>
-                </Col>
-                <Col xs={6} md={4}>
+                        </div>
+                    </Link>
+                        <div className="gallImage">
                     <Image fluid src={bild15} alt='Img 1' className='gallery-img'></Image>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={6} md={4}>
-                    <Image fluid src={bild12} alt='Img 1' className='gallery-img'></Image>
-                </Col>
-                <Col xs={6} md={4}>
-                    <Image fluid src={bild13} alt='Img 1' className='gallery-img'></Image>
-                </Col>
-                <Col xs={6} md={4}>
-                    <Image fluid src={bild15} alt='Img 1' className='gallery-img'></Image>
+                        </div>
+                        <div className="gallImage">
+                            <Image fluid src={bild13} alt='Img 1' className='gallery-img'></Image>
+                        </div>
                 </Col>
             </Row>
             </Container>
---
+++ multimedia_react/src/components/Product1.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import '../App.css';
+import { Container } from "react-bootstrap";
+
+function Product1() {
+    return(
+        <Container fluid className="background">
+        <div>
+
+        </div>
+        </Container>
+    )
+}
+
+export default Product1;
