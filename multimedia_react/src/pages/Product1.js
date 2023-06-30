ee0eeef694dac7a94b097600d17eca81fbe8f46f
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-30 08:22:12 +0200

    finished Product1 page

--- multimedia_react/src/pages/Product1.js
+++ multimedia_react/src/pages/Product1.js
@@ -15,7 +15,28 @@ function Product1() {
       </Col>
       <Col>
         <div>
-          <h1>kjesfh</h1>
+          <h1 className="product-title">Stehlampe</h1>
+          <br></br>
+          <h4 className="price">CHF 69.-</h4>
+          <br></br>
+          <p className="product-description">Unsere Stehlampe ist die perfekte Ergänzung für Ihr Zuhause. Mit ihrem eleganten Design und ihrer funktionalen Beleuchtung schafft sie eine angenehme Atmosphäre in jedem Raum.</p>
+          <p className="product-description">Mit ihrem zeitlosen Stil fügt sich die Stehlampe harmonisch in verschiedene Einrichtungsstile ein. Sie kann als Leselampe neben Ihrem Lieblingssessel oder als stimmungsvolle Beleuchtung in einer Ecke des Wohnzimmers verwendet werden. Dank des praktischen Ein- und Ausschalters lässt sich die Lampe bequem bedienen.</p>
+          <Row>
+            <Col>
+              <p className="info">Material</p>
+            </Col>
+            <Col>
+              <p className="info2">Holz/Metall</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className="info">Farbe</p>
+            </Col>
+            <Col>
+              <p className="info2">Gold</p>
+            </Col>
+          </Row>
         </div>
       </Col>
       </Row>
