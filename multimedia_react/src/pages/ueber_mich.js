e9a3b8396163bd0b30ed87a20c9845df337e7ccd
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-06-30 09:55:13 +0200

    finished ueber_mich page

--- multimedia_react/src/pages/ueber_mich.js
+++ multimedia_react/src/pages/ueber_mich.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import bild2 from "../Images/7.jpg";
 
-function uebermich() {
-    return (
-        <Container fluid className="background">
-        <div>
-            <h1 className="title"> 
-                Our Story 
-            </h1>
-            <Row className="upper-background">
-               <p className="text">Welcome to our online lamp shop! We are passionate about lighting up your life with beautiful and
-        captivating lamps. Our journey began with a simple idea - to provide unique and stylish lighting solutions
-        that transform any space into a warm and inviting haven.</p>
-            </Row>
-            <Row className="lower-background">
-                <h1>jdf</h1>
-            </Row>
-        </div>
-        </Container>
-    );
+function Uebermich() {
+  return (
+    <Container fluid className="background">
+      <div className="about-background">
+        <Row className="upper-background">
+          <Col>
+            <img src={bild2} alt="Lamp" className="lamp-image" />
+          </Col>
+          <Col>
+          <h1 className="about-title">Our Story</h1>
+            <p className="text">
+              Welcome to our online lamp shop! We are passionate about lighting up your life with beautiful and captivating lamps. Our journey began with a simple idea - to provide unique and stylish lighting solutions that transform any space into a warm and inviting haven.ehind the scenes, a team of skilled artisans worked diligently to craft these illuminating masterpieces. They poured their hearts and souls into each lamp, infusing them with love and care. The artisans believed that their creations had the power to transform spaces, create ambiance, and evoke emotions.
+            </p>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
 }
 
-export default uebermich;
+export default Uebermich;
